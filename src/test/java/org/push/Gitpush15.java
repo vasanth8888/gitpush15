7daee7aca36999e5974140e8bc4b45808792bbca
vasanth8888 <vasanthveeda09@gmail.com>
2022-04-05 15:08:52 +0530

    gitpush15

---
+++ src/test/java/org/push/Gitpush15.java
@@ -0,0 +1,26 @@
+package org.push;
+
+public class Gitpush15 {
+	
+	
+	private void name1() {
+		// TODO Auto-generated method stub
+  System.out.println(1);
+	}
+	
+	private void name2() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
